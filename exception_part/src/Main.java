9df9ecc7af6c10cb0c345af18bc3aa48ff2d5906
rustam <rustamaxm@gmail.com>
2024-12-19 13:52:11 +0300

    logger done

--- exception_part/src/Main.java
+++ exception_part/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.logging.*;
+
 public class Main {
     public static double sqrt(double x) {
         if (x < 0) {
@@ -5,9 +7,33 @@ public class Main {
         }
         return Math.sqrt(x);
     }
+
+    private static void configureLogging() {
+        // your implementation here
+        Logger LOGGER1 = Logger.getLogger("org.stepic.java.logging.ClassA");
+        LOGGER1.setLevel(Level.ALL);
+
+        Logger LOGGER2 = Logger.getLogger("org.stepic.java.logging.ClassB");
+        LOGGER2.setLevel(Level.WARNING);
+
+        Logger LOGGER3 = Logger.getLogger("org.stepic.java");
+        ConsoleHandler handler = new ConsoleHandler();
+        handler.setLevel(Level.ALL);
+        handler.setFormatter(new XMLFormatter());
+        LOGGER3.addHandler(handler);
+        LOGGER3.setUseParentHandlers(false);
+    }
+    
     public static void main(String[] args) {
 
         System.out.println(sqrt(2));
-        System.out.println(sqrt(-1));
+        try {
+            System.out.println(sqrt(-1));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        Logger log = Logger.getLogger("demo");
+        log.log(Level.INFO, "from logger");
     }
 }
